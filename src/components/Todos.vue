0d6d870c14e1b5d97e025debebbdb575732d62d9
Saltion <saltionsatka@gmail.com>
2020-05-27 14:32:22 +0200

    First commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+       <div v-for="todo in todoList" :key="todo.id">{{ todo.title }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Todos",
+        props: ["todoList"]
+    }
+</script>
+
+<style scoped>
+
+</style>
